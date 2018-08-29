0e634a7163e8f5cde5d33a6dc236245b70f61fa4
https://vishalkonline@bitbucket.org <vishalkonline@gmail.com>
2018-08-29 16:03:35 +0530

    clean gh-pages

--- src/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const createActions = Model => ({
-  getUserLocation: () => Model.present({ getUserLocation: true }),
-  onMapReady: (mapProps, map) => Model.present({ onMapReady: { mapProps, map } }),
-  searchNearby: types => Model.present({ searchNearby: types }),
-  activeLeftLink: name => Model.present({ activeLeftLink: name }),
-  showHideLeftNav: () => Model.present({ showHideLeftNav: true }),
-});
--- src/component/index.js
+++
@@ -1,9 +0,0 @@
-import { header } from './header';
-import { typesLists } from './typesLists';
-import { placesLists } from './placesLists';
-
-export {
-  header,
-  typesLists,
-  placesLists
-};
